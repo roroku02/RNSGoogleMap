3491083ea038763363d15e806b96cf2a254cc1c0
roroku02 <roroku02@gmail.com>
2019-01-04 23:52:42 +0900

    複数マーカーの追加と削除

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var infoWindow;
 var i = 0;
 
 function initMap() {
-    map_default = new google.maps.LatLng(35.01, 135.867);
+    map_default = new google.maps.LatLng(34.851747492179066, 135.6176956788463);
 
     map = new google.maps.Map(document.getElementById("map"), {
         center: map_default,
@@ -31,24 +31,27 @@ function initMap() {
             map: map,
             draggable: true
         });
-
-        infoWindow = new google.maps.InfoWindow({
-            content: '<a href="#" onclick="clear_marker(' + i + ')">マーカーを削除</a>'
-        });
         my_marker[i].setMap(map);
-
-        my_marker[i].addListener('click', function () {
-            infoWindow.open(map, my_marker[i]);
-        });
 
         google.maps.event.addListener(my_marker[i], 'dragend', function (event) {
             alert(event.latLng.lat() + '\n' + event.latLng.lng());
         });
+
+        attachMassage(my_marker[i], '<a href="#" onclick="clear_marker(' + i + ')">マーカーを削除</a>');
     });
 
 }
 
 function clear_marker(num) {
     my_marker[num].setMap(null);
+    my_marker[num] = null;
 };
+
+function attachMassage(marker, msg) {
+    google.maps.event.addListener(marker, 'click', function (event) {
+        new google.maps.InfoWindow({
+            content: msg
+        }).open(marker.getMap(), marker);
+    });
+}
 window.onload = initMap();
