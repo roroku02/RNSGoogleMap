6e5225e72a9faafe81740b4991b701ec67e3c9dd
roroku02 <roroku02@gmail.com>
2019-06-05 20:59:53 +0900

    水路に色を塗る

--- main.js
+++ main.js
@@ -6,19 +6,6 @@ var center_lng = '135.6176956788463';
 //カスタムマーカーのデフォルトアイコン
 var default_icon;
 
-function initMap() {
-    //マップデフォルト位置設定（JR高槻駅）
-    //map_default = new google.maps.LatLng(34.851747492179066, 135.6176956788463);
-    map_default = { lat: 34.851747492179066, lng: 135.6176956788463 };
-    //マップ表示
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: map_default,
-        zoom: 18,
-    });
-
-    map.setOptions({ styles: styles["clear"] });
-}
-
 var styles = {
     clear: [
         {
@@ -183,5 +170,177 @@ var styles = {
                 }
             ]
         }
+    ],
+    water: [
+        {
+            "featureType": "all",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "weight": "2.00"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#9c9c9c"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text",
+            "stylers": [
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#f2f2f2"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                },
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "saturation": -100
+                },
+                {
+                    "lightness": 45
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#eeeeee"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#7b7b7b"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#46bcec"
+                },
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#070707"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        }
     ]
 };
+
+function initMap() {
+    //マップデフォルト位置設定（JR高槻駅）
+    //map_default = new google.maps.LatLng(34.851747492179066, 135.6176956788463);
+    map_default = { lat: 34.851747492179066, lng: 135.6176956788463 };
+    //マップ表示
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: map_default,
+        zoom: 18,
+    });
+
+}
+
+function changeMap(MapType) {
+    map.setOptions({ styles: styles[MapType] });
+}
+
